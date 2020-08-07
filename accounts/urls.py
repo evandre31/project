37b58dadf29a198b104c987b25ad24fd05c08074
foued <foued>
2020-08-08 00:36:52 +0200

    login and logout finish, model profile and signal

--- accounts/urls.py
+++ accounts/urls.py
@@ -14,10 +14,15 @@ app_name = 'accounts'
 # ]
 
 urlpatterns = [
-    # path('', views.home, name='home'),
     path('login/',
          auth_views.LoginView.as_view(template_name='accounts/registration/login.html'),
          name='login'),
+
+    path('logout/',
+         auth_views.LogoutView.as_view(template_name='accounts/registration/logged_out.html'),
+         name='logout'),
+
+
     # path('signout/', views.signout, name='signout'),
     # path('signup/', views.signup, name='signup'),
     path('profile/', views.profile, name='profile'),
